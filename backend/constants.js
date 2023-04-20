654cfb619fa880b9ffd9415bddcd79b6dce40859
CryptoNube <daverad@hotmail.com>
2023-04-20 12:58:44 -0500

    first commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xcb2b47d1906258Bf66cbfd35Fb824547BcEa08C9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
